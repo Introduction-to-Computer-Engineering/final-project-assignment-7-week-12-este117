220deb8529cce3f6973966d49f062a2e70db0ce1
este117 <54929311+este117@users.noreply.github.com>
2019-11-05 14:02:39 -0700

    Update enable-matrix.js

--- enable-matrix.js
+++ enable-matrix.js
@@ -1,33 +1,32 @@
-led.enable(true)
-pins.analogWritePin(AnalogPin.P6, 0)
-pins.analogWritePin(AnalogPin.P8, 0)
+pins.analogWritePin(AnalogPin.P8, 0) 8 12 16
+pins.analogWritePin(AnalogPin.P12, 0)
 pins.analogWritePin(AnalogPin.P9, 0)
 basic.forever(function () {
     for (let index = 0; index <= 3071; index++) {
         if (index < 2046) {
             if (index <= 1023) {
-                pins.analogWritePin(AnalogPin.P6, index)
+                pins.analogWritePin(AnalogPin.P8, index)
             } else {
-                pins.analogWritePin(AnalogPin.P6, 1023 - (index - 1023))
-            }
-        } else {
-            pins.analogWritePin(AnalogPin.P6, 0)
-        }
-        if (index > 1023) {
-            if (index <= 2046) {
-                pins.analogWritePin(AnalogPin.P8, index - 1023)
-            } else {
-                pins.analogWritePin(AnalogPin.P8, 1023 - (index - 2047))
+                pins.analogWritePin(AnalogPin.P8, 1023 - (index - 1023))
             }
         } else {
             pins.analogWritePin(AnalogPin.P8, 0)
         }
-        if (index < 1023) {
-            pins.analogWritePin(AnalogPin.P9, 1022 - index)
-        } else if (index > 2048) {
-            pins.analogWritePin(AnalogPin.P9, index - 2048)
+        if (index > 1023) {
+            if (index <= 2046) {
+                pins.analogWritePin(AnalogPin.P12, index - 1023)
+            } else {
+                pins.analogWritePin(AnalogPin.P12, 1023 - (index - 2047))
+            }
         } else {
-            pins.analogWritePin(AnalogPin.P9, 0)
+            pins.analogWritePin(AnalogPin.P12, 0)
+        }
+        if (index < 1023) {
+            pins.analogWritePin(AnalogPin.P16, 1022 - index)
+        } else if (index > 2048) {
+            pins.analogWritePin(AnalogPin.P16, index - 2048)
+        } else {
+            pins.analogWritePin(AnalogPin.P16, 0)
         }
         control.waitMicros(1000)
     }
